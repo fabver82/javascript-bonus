b3b0d64c6595599d4ee86f02e04acc7a2e78488b
Fabien V <fabien.vermeiren@gmail.com>
2022-02-24 14:25:36 +0100

    generate table

--- bonus/08-dom/07-generate-table-one/script.js
+++ bonus/08-dom/07-generate-table-one/script.js
@@ -10,7 +10,16 @@
 // You will have time to focus on it later.
 
 (function() {
-
+    let tbl = document.createElement("table");
+    for (let i =1;i<=10;i++){
+        let col = document.createElement('tr');
+        let row = document.createElement('td');
+        tbl.appendChild(col);
+        row.innerHTML='row '+i;
+        col.appendChild(row);
+    }
+    let target = document.getElementById('target');
+    target.appendChild(tbl);
     // your code here
 
 })();
